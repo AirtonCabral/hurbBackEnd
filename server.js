ee17beed9c8f4b760648f5cddd82b023ada2df98
AirtonCabral <designnoturno@gmail.com>
2019-08-23 12:52:06 -0300

    adicionado gitignore

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
 const express = require('express');
-var request = require('request');
+const request = require('request');
 const app = express();
 let cors = require('cors');
+
+const PORT = 3000;
+const HOST = '0.0.0.0'
+
 app.use(cors());
 const url ='https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=pt-BR';
 
 app.get('/image', function(req, res){
     request(url, function (error, response, body) {
-        // console.log('error:', error); // Print the error if one occurred
-        // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-        // console.log('body:', body); // Print the HTML for the Google homepage.
-        
         res.status(200).send(body)
     });
 })
 
 
 
-app.listen(3003)
+app.listen(PORT, HOST)
